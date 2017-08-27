d5e97eddade6df785df0d20ed5c0a91e19a7f950
luizcarlospedrosogomes <lc.pg@hotmail.com>
2017-08-26 22:29:27 -0300

    heroku

--- apf-scrum-cli/src/Login/Login.js
+++ apf-scrum-cli/src/Login/Login.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 import FormCadastro from './FormCadastro';
 
 export default  class Login extends Component{
-    
+    constructor(props) {
+        super(props);
+        this.state = {msg: '', status:'', cod:''};
+    }
       login(event){
+        console.log(this.email.value);
         event.preventDefault();
         this.setState({status:"carregando"});
         const requestInfo = {
@@ -11,32 +15,27 @@ export default  class Login extends Component{
             body:JSON.stringify({email: this.email.value, senha:this.senha.value}),
             headers:new Headers({'content-type' : 'application/json'})
         };
-        console.log("SERVIDOR: "+ this.host);
-        console.log("URL :"+this.baseUrl+"/login")
-        console.log("DADOS ENVIADOS: "+requestInfo.body);
-        console.log("VERBO: POST")
-        
-        fetch(this.host+this.baseUrl+"/login",requestInfo)            
+
+        fetch("http://scrum-php.herokuapp.com/v1/login",requestInfo)            
             .then(response =>{
             if(response.status === 200 ||  response.status === 201){
                 this.setState({cod:response.status, status:""});
-                console.log("sucesso no login");
                 return response.text();
             }else{
                 this.setState({status:"", cod:response.status});
                 throw new Error('Não foi possivel fazer o login. Verifique usuario e senha.');
             }
-        }).then(token =>{
-            console.log(token)
-            if(this.props.match.params.login === 'representante'){
-                localStorage.setItem('token-representante',token);
-                localStorage.setItem('email-representante',this.email.value);
-                this.props.history.push('/representante');
-            }else if(this.props.match.params.login === 'cliente'){
-                localStorage.setItem('token-cliente',token);
-                localStorage.setItem('email-cliente', this.email.value);
-                this.props.history.push('/cliente/'+this.email.value);
-            }
+        }).then(dados =>{
+            console.log(dados.tipousuario);
+            console.log(dados.jwt);
+            localStorage.setItem('token',dados.jwt);
+            if(dados.tipousuario == 1)
+                 this.props.history.push('/productOwner');
+            if(dados.tipousuario == 2)
+                this.props.history.push('/scrumMaster');
+            if(dados.tipousuario == 3)
+                this.props.history.push('/desenvolvedor');
+            
         }).catch(error => {
             this.setState({msg:error.message, cod:500, status:""});
         })
@@ -47,7 +46,7 @@ export default  class Login extends Component{
          return(
             <div>
                 <div className="header">
-                    <h1>Gerenciador Projetos Scrum</h1>
+                    Gerenciador Projetos Scrum
                 </div>
 
                 <div className="row">
@@ -58,10 +57,23 @@ export default  class Login extends Component{
                             </div>
                             
                             <div className="panel-body">
-                                <form className="form-group">
-                                    <input className="form-control" type="text" placeholder="Email" required="true"/>
-                                    <input className="form-control" type="password"  placeholder="Senha" required="true"/>
-                                    <button type="submit" className=" btn-fill btn-block btn btn-info btn-lg">Entrar</button>                       
+                                <form className="form-group" onSubmit={this.login.bind(this)}>
+                                    <input className="form-control" 
+                                         type="email"
+                                         placeholder="Email"
+                                         ref={(input) => this.email = input} 
+                                         required="true"
+                                    />
+                                    <input className="form-control" 
+                                            type="password"  
+                                            placeholder="Senha" 
+                                            ref={(input) => this.senha = input}
+                                            required="true"
+                                    />
+                                    <button type="submit" 
+                                            className=" btn-fill btn-block btn btn-info btn-lg">
+                                            Entrar
+                                    </button>                       
                                 </form>
 
                             </div>
